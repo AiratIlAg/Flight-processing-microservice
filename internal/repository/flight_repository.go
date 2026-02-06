f0f0dc9dc4d113e07e9cdb98d8068b2c105c5e8d
Airat <agliullov_airat@mail.ru>
2026-02-06 16:43:24 +0400

    add stage 2 and 3

---
+++ internal/repository/flight_repository.go
@@ -0,0 +1,90 @@
+package repository
+
+import (
+	"context"
+	"flight_processing/internal/service"
+	"time"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type FlightRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewFlightRepository(db *pgxpool.Pool) *FlightRepository {
+	return &FlightRepository{db: db}
+}
+
+func (r *FlightRepository) Upsert(ctx context.Context, f *service.FlightData) error {
+	query, args, err := psql.
+		Insert("flights").
+		Columns(
+			"flight_number",
+			"departure_date",
+			"aircraft_type",
+			"arrival_date",
+			"passengers_count",
+			"updated_at",
+		).
+		Values(
+			f.FlightNumber,
+			f.DepartureDate,
+			f.AircraftType,
+			f.ArrivalDate,
+			f.PassengersCount,
+			time.Now(),
+		).
+		Suffix(`
+			ON CONFLICT (flight_number, departure_date)
+			DO UPDATE SET
+				aircraft_type = EXCLUDED.aircraft_type,
+				arrival_date = EXCLUDED.arrival_date,
+				passengers_count = EXCLUDED.passengers_count,
+				updated_at = EXCLUDED.updated_at
+		`).
+		ToSql()
+	if err != nil {
+		return err
+	}
+
+	_, err = r.db.Exec(ctx, query, args...)
+	return err
+}
+
+func (r *FlightRepository) Get(ctx context.Context, flightNumber string, departureDate time.Time) (*service.FlightData, error) {
+	query, args, err := psql.
+		Select(
+			"aircraft_type",
+			"flight_number",
+			"departure_date",
+			"arrival_date",
+			"passengers_count",
+			"updated_at",
+		).
+		From("flights").
+		Where(sq.Eq{
+			"flight_number":  flightNumber,
+			"departure_date": departureDate,
+		}).
+		ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	var f service.FlightData
+	err = r.db.QueryRow(ctx, query, args...).Scan(
+		&f.AircraftType,
+		&f.FlightNumber,
+		&f.DepartureDate,
+		&f.ArrivalDate,
+		&f.PassengersCount,
+		&f.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	return &f, nil
+}
